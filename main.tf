4e1250f0dc598db5f313100b2ae4ae1b9d41b936
sachinnegi001 <sachinnegi071@gmail.com>
2023-06-27 20:09:29 +0530

    after modifying

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@ resource "newrelic_alert_policy" "alert" {
 # Add a condition
 resource "newrelic_nrql_alert_condition" "FirstTest" {
   policy_id                    = newrelic_alert_policy.alert.id
-  type                         = "static"
-  name                         = "FirstTest"
-  description                  = "Alert when transactions are taking too long"
+  type                         = var.type
+  name                         = var.Name
+  description                  = var.discription
   runbook_url                  = "https://www.example.com"
   enabled                      = true
-  violation_time_limit_seconds = 3600
+  violation_time_limit_seconds = var.violation_time_limit_seconds
 
   nrql {
     query             = "SELECT average(host.cpuPercent) AS 'CPU used %' FROM Metric WHERE `entityGuid` = 'MzkzMzUyOHxJTkZSQXxOQXwyMjc2MjE3MDc2MTMwMzAzMTA'"
   }
 
   critical {
-    operator              = "above"
-    threshold             = 5.5
-    threshold_duration    = 300
+    operator              = var.operator
+    threshold             = var.Thrushold
+    threshold_duration    = var.Thrushold_duration
     threshold_occurrences = "ALL"
   }
 }
